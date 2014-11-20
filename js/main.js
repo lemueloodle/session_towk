16f5e1c38dc806f0bab1c1edd7d618e53559c63d
lemueloodle <ljpaule@gmail.com>
2014-11-20 10:37:22 +0800

    Sample Query/Return Callback JSON

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 $(document).ready(function(){
  
-	var x = $('body').attr('dtx-ox'); //Get the value of session 
+  $(document).on("click", ".x_send", function(){
+  	var x = $(this).attr('pox-ms'); //Get the value of session token
+    var y = $('#x_text').val(); //Get the value of the text box
   	arrayx = []; //Create an array for json
-	$.ajax({
-      url: '../php/query.php',
+    $.ajax({
+      url: './php/query.php',
       data: {
-        key: x
+        key: x,
+        e_data: y 
       },
       type: 'POST',
       cache: false,
       dataType: "json",
       success: function(data){
-           $.each(dataT, function(key, val){
+           $.each(data, function(key, val){
               arrayx.push(data[key]);
           });
+          $('.x_display').html(arrayx[0]);
        }
    });
-
+  });
 });
--- php/query.php
+++ php/query.php
@@ -1,7 +1,51 @@
 <?php
 session_start();
-
 include('anthrax.php');
 
-  
+if(isset($_POST['key'])){
+	$salted_token = clean_input($_POST['key']); //Clean input from CSRF Attack
+	$salted_token = mysql_real_escape_string($salted_token); //A little prevention from injection
+	$salted_token = addslashes($salted_token);
+}
+else
+	$salted_token = "";
+
+if(isset($_POST['e_data'])){
+	$passed_input = clean_input($_POST['e_data']); //Clean input from CSRF Attack
+	$passed_input = mysql_real_escape_string($passed_input); //A little prevention from injection
+	$passed_input = addslashes($passed_input);
+}
+else
+	$passed_input = "";
+
+//If passed values are not empty
+if($salted_token != "" && $passed_input != ""){
+	
+	//The number should be equal to the SESSION token number of string
+	$trimmed_secret = substr($salted_token, 50);
+
+	//Decrypt the Secret Key
+	$secret_key = decryptx("{$trimmed_secret}");
+
+	//Trimmed Session Token
+	$session_token = stristr($salted_token, $trimmed_secret, true);
+
+	//For the purpose of double checking of conditions;
+	//This also serves as to compare the secret key from mainpage if correct
+	if($_SESSION['S3SX_token'] == $session_token && $secret_key == "secretkey"){
+		echo "{";
+		echo '"key" : "'.$passed_input.'"';
+		echo "}";
+
+		//INPUT YOUR QUERY HERE
+	}
+	else{
+		echo "{";
+		echo '"key" : "?"';
+		echo "}";
+	}
+}
+else{
+	header("Location: ../index.php");
+}
 ?>
